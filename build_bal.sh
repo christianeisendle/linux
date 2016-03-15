8824d4e7843c34de295a2038c586136121bc00b7
Christian Eisendle <christian@eisendle.net>
2016-03-15 16:42:39 +0000

    Make sure include/config/kernel.release is available
    in build_bal.sh, include/config/kernel.release is read out to get the kernel version.
    However, this file is not available on a clean kernel tree.
    
    This changes build_bal.sh to first call make modules_prepare which generates the file.

--- build_bal.sh
+++ build_bal.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 NUM_CPU_CORES=`/usr/bin/nproc`
+
+KERNEL=kernel7
+make -j$NUM_CPU_CORES bcm2709_defconfig
+make -j$NUM_CPU_CORES modules_prepare
+
 KERNELRELEASE=`cat include/config/kernel.release`
 CURRENT_KERNEL_VERSION=`uname -r`
 CURRENT_KERNEL_SHORT_VERSION=`uname -r | sed -e "s/\([0-9]*.[0-9]*.[0-9]*\).*/\1/"`
@@ -10,9 +15,6 @@ else
 	echo "Error: Module.symvers for current kernel ($CURRENT_KERNEL_SHORT_VERSION) not found. Check symvers/ folder for available versions or get the version for your kernel from https://github.com/raspberrypi/firmware/tree/master/extra"
 	exit 1
 fi
-KERNEL=kernel7
-make -j$NUM_CPU_CORES bcm2709_defconfig
-make -j$NUM_CPU_CORES modules_prepare
 make -j$NUM_CPU_CORES M=$PWD/drivers/bal && sudo make M=$PWD/drivers/bal modules_install
 
 # copy also to current kernel dir
